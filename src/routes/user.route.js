efb192f29fe7067632e5637fc54f86e3a752d90a
Codev00 <kenngok2@gmail.com>
2023-07-27 09:50:16 +0700

    add middleware for user.route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -111,6 +111,7 @@ router.post(
    body("mediaTitle").exists().withMessage("mediaId is required"),
    body("mediaPoster").exists().withMessage("mediaPoster is required"),
    body("mediaRate").exists().withMessage("mediaRate is required"),
+   requestHandler.validate,
    favoriteController.addFavorite
 );
 
