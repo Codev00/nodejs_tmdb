f2e0833516e7146186876427bf23f066cd4bb039
Codev00 <kenngok2@gmail.com>
2023-07-27 09:56:10 +0700

    add root patch route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import userRoute from "../routes/user.route.js";
+import mediaRoute from "../routes/media.route.js";
+import reviewRoute from "../routes/review.route.js";
+import personRoute from "../routes/person.route.js";
 
 const router = express.Router();
 
 router.use("/user", userRoute);
+router.use("/:mediaType", mediaRoute);
+router.use("/review", reviewRoute);
+router.use("/person", personRoute);
 
 export default router;
